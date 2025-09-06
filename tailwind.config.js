53eafcaa05d2b5258bf5f79362fee5fea6d6f62f
Maria Vitória <mariavitoriabarreto2@gmail.com>
2025-09-06 14:50:44 -0300

    css arrumado

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,3 +81,14 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
